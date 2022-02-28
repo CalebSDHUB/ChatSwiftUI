110aeb7e8e01ba43838ca6bd3695cc1e092b83d1
Caleb Danielsen <caleb@lfpost.dk>
2022-02-28 10:42:44 +0100

    Initial Commit

---
+++ ChatSwiftUI/ChatSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatSwiftUIApp.swift
+//  ChatSwiftUI
+//
+//  Created by Caleb Danielsen on 28/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
